dd9c1e0268b0a8ca58360a5eb23404a24b4c4b6d
Josh Davis <j@joshldavis.com>
2016-04-11 06:47:12 -0700

    Add brackets/braces to Code layer

--- keymap.c
+++ keymap.c
@@ -51,7 +51,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    KC_GRV,        KC_1,        KC_2,  KC_3, KC_4,   KC_5, KC_TRNS,
    KC_TAB,        KC_Q,        KC_W,  KC_E, KC_R,   KC_T, KC_LBRC,
    CTL_T(KC_ESC), KC_A,        KC_S,  KC_D, KC_F,   KC_G,
-   KC_LSFT,       KC_Z,        KC_X,  KC_C, KC_V,   KC_B, MO(CODE),
+   KC_LSFT,       KC_Z,        KC_X,  KC_C, KC_V,   KC_B, KC_TRNS,
    KC_LCTL, MEH_T(KC_NO), ALL_T(KC_NO), KC_LALT, KC_LGUI,
                                         KC_TRNS, KC_TRNS,
                                                  KC_TRNS,
@@ -61,7 +61,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                                                 KC_MINS, KC_6, KC_7,    KC_8,    KC_9,        KC_0,          KC_EQL,
                                                                 KC_RBRC, KC_Y, KC_U,    KC_I,    KC_O,        KC_P,          KC_BSLS,
                                                                          KC_H, KC_J,    KC_K,    KC_L,        KC_SCLN,       CTL_T(KC_QUOT),
-                                                                MO(CODE), KC_N, KC_M,    KC_COMM, KC_DOT,      KC_SLSH,       KC_RSFT,
+                                                                KC_TRNS, KC_N, KC_M,    KC_COMM, KC_DOT,      KC_SLSH,       KC_RSFT,
                                                                                KC_SPC,  MO(MDIA),TG(CODE),    TG(CLMK),      TG(MDIA),
                                                                 KC_TRNS, KC_TRNS,
                                                                 KC_TRNS,
@@ -73,11 +73,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * ,--------------------------------------------------.           ,--------------------------------------------------.
  * |  Esc   |  F1  |  F2  |  F3  |  F4  |  F5  |      |           |      |  F6  |  F7  |  F8  |  F9  |  F10 |   F11  |
  * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
- * |        |      |      |      |      |      |      |           |      |      |      |      |      |      |   F12  |
+ * |        |      |      |      |      |      |      |           |      |      |   -  |   =  |   [  |   ]  |   F12  |
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
  * |        |   !  |   @  |   #  |   $  |   %  |------|           |------|   ^  |   &  |   *  |   (  |   )  |        |
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
- * |        |      |      |      |      |      |      |           |      |      |      |      |      |      |        |
+ * |        |      |      |      |      |      |      |           |      |      |      |      |   {  |   }  |        |
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
  *   |      |      |      |      |      |                                       |      |      |      |      |      |
  *   `----------------------------------'                                       `----------------------------------'
@@ -102,9 +102,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                                    KC_TRNS,KC_TRNS,KC_TRNS,
                                                                      // right hand
                                                                      KC_TRNS,      KC_F6,   KC_F7,   KC_F8,     KC_F9,   KC_F10,  KC_F11,
-                                                                     KC_TRNS,      KC_TRNS, KC_TRNS, KC_TRNS,   KC_TRNS, KC_TRNS, KC_F12,
+                                                                     KC_TRNS,      KC_TRNS, KC_MINS, KC_EQL,    KC_LBRC, KC_RBRC, KC_F12,
                                                                                    KC_CIRC, KC_AMPR, KC_ASTR,   KC_LPRN, KC_RPRN, KC_TRNS,
-                                                                     KC_TRNS,      KC_TRNS, KC_TRNS, KC_TRNS,   KC_TRNS, KC_TRNS, KC_TRNS,
+                                                                     KC_TRNS,      KC_TRNS, KC_TRNS, KC_TRNS,   KC_LCBR, KC_RCBR, KC_TRNS,
                                                                                             KC_TRNS,  KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS,
                                                                      KC_TRNS, KC_TRNS,
                                                                      KC_TRNS,
