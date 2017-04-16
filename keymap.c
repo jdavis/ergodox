60a350de78bac5329628465bcf50649432b79500
Josh Davis <j@joshldavis.com>
2017-04-15 21:33:52 -0700

    Playing with coding layer a bit

--- keymap.c
+++ keymap.c
@@ -73,9 +73,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * ,--------------------------------------------------.           ,--------------------------------------------------.
  * |  Esc   |  F1  |  F2  |  F3  |  F4  |  F5  |      |           |      |  F6  |  F7  |  F8  |  F9  |  F10 |   F11  |
  * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
- * |        |      |      |      |      |      |      |           |      |      |   -  |   =  |   [  |   ]  |   F12  |
+ * |        |   !  |   @  |   #  |   $  |   %  |      |           |      |   ^  |   &  |   *  |   (  |   )  |   F12  |
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
- * |        |   !  |   @  |   #  |   $  |   %  |------|           |------|   ^  |   &  |   *  |   (  |   )  |        |
+ * |        |   1  |   2  |   3  |   4  |   5  |------|           |------|   6  |   7  |   8  |   9  |   0  |        |
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
  * |        |      |      |      |      |      |      |           |      |      |      |      |   {  |   }  |        |
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
@@ -93,8 +93,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [CODE] = KEYMAP(
        // left hand
        KC_ESC,  KC_F1,    KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_TRNS,
-       KC_TRNS, KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-       KC_TRNS, KC_EXLM,  KC_AT,   KC_HASH, KC_DLR,  KC_PERC,
+       KC_TRNS, KC_EXLM,  KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_TRNS,
+       KC_TRNS, KC_1,     KC_2,    KC_3,    KC_4,    KC_5,
        KC_TRNS, KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
        KC_TRNS, KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS,
                                                          KC_TRNS,KC_TRNS,
@@ -102,8 +102,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                                    KC_TRNS,KC_TRNS,KC_TRNS,
                                                                      // right hand
                                                                      KC_TRNS,      KC_F6,   KC_F7,   KC_F8,     KC_F9,   KC_F10,  KC_F11,
-                                                                     KC_TRNS,      KC_TRNS, KC_MINS, KC_EQL,    KC_LBRC, KC_RBRC, KC_F12,
-                                                                                   KC_CIRC, KC_AMPR, KC_ASTR,   KC_LPRN, KC_RPRN, KC_TRNS,
+                                                                     KC_TRNS,      KC_CIRC, KC_AMPR, KC_ASTR,   KC_LPRN, KC_RPRN, KC_F12,
+                                                                                   KC_6,    KC_7,    KC_8,      KC_9,    KC_0,    KC_TRNS,
                                                                      KC_TRNS,      KC_TRNS, KC_TRNS, KC_TRNS,   KC_LCBR, KC_RCBR, KC_TRNS,
                                                                                             KC_TRNS,  KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS,
                                                                      KC_TRNS, KC_TRNS,
