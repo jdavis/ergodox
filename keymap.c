06f04ad0430ffe8b58a92da2f08b3e1ce6f5445b
Josh Davis <j@joshldavis.com>
2017-04-16 10:20:16 -0700

    Remove unused tap modifiers, clean up ASCII

--- keymap.c
+++ keymap.c
@@ -20,48 +20,44 @@
 /*
  * @jdavis Ergodox EZ layout
  *
- * Original layout by Nicholas Keene:
- *     http://nicholas.rinard.us/2016/03/ergodox-ez-layout.html
- *
  * Issues:
  *     - Code button isn't in a very great spot
  *          - Fix: Move to a place that is symmetrical
  *     - The minus key (-) is in a really terrible spot
  *          - Fix: Put it in a good place on the coding layer (this relies on the above fix as well)
  *     - Maybe use semicolon for a modifier?
- *
  */
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 /* Keymap 0: Basic layer
  *
- * ,--------------------------------------------------.           ,--------------------------------------------------.
- * |   ~    |   1  |   2  |   3  |   4  |   5  |      |           |  -   |   6  |   7  |   8  |   9  |   0  |   +    |
- * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
- * | Tab    |   Q  |   W  |   E  |   R  |   T  |   [  |           |  ]   |   Y  |   U  |   I  |   O  |   P  |   \    |
- * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
- * |ESC/CTRL|   A  |   S  |   D  |   F  |   G  |------|           |------|   H  |   J  |   K  |   L  |  ;   | '/CTRL |
- * |--------+------+------+------+------+------|   (  |           |  )   |------+------+------+------+------+--------|
- * | LShift |   Z  |   X  |   C  |   V  |   B  |      |           |      |   N  |   M  |   ,  |   .  |  /   | RShift |
- * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
- *   |LCtrl | LAlt |      | LAlt | LGui |                                       | Space| ~Mdia| LGui | LAlt | RCtrl|
- *   `----------------------------------'                                       `----------------------------------'
- *                                      ,---------------.       ,---------------.
- *                                      |        |      |       | Code |        |
- *                               ,------|--------|------|       |------+--------+------.
- *                               |      |        |      |       |Colemk|        |      |
- *                               |Bkspc |   Del  |------|       |------| ~Code  | Enter|
- *                               |      |        | Space|       | Mdia |        |      |
- *                               `----------------------'       `----------------------'
+ * ,--------------------------------------------------.       ,--------------------------------------------------.
+ * |   ~    |   1  |   2  |   3  |   4  |   5  |      |       |  -   |   6  |   7  |   8  |   9  |   0  |   +    |
+ * |--------+------+------+------+------+-------------|       |------+------+------+------+------+------+--------|
+ * | Tab    |   Q  |   W  |   E  |   R  |   T  |   [  |       |  ]   |   Y  |   U  |   I  |   O  |   P  |   \    |
+ * |--------+------+------+------+------+------|      |       |      |------+------+------+------+------+--------|
+ * |  ESC   |   A  |   S  |   D  |   F  |   G  |------|       |------|   H  |   J  |   K  |   L  |  ;   |   '    |
+ * |--------+------+------+------+------+------|   (  |       |  )   |------+------+------+------+------+--------|
+ * | LShift |   Z  |   X  |   C  |   V  |   B  |      |       |      |   N  |   M  |   ,  |   .  |  /   | RShift |
+ * `--------+------+------+------+------+-------------'       `-------------+------+------+------+------+--------'
+ *   |LCtrl | LAlt |      | LAlt | LGui |                                   | Space| ~Mdia| LGui | LAlt | RCtrl|
+ *   `----------------------------------'                                   `----------------------------------'
+ *                                    ,---------------.       ,---------------.
+ *                                    |        |      |       |      |        |
+ *                             ,------|--------|------|       |------+--------+------.
+ *                             |      |        |      |       |      |        |      |
+ *                             |Bkspc |   Del  |------|       |------| ~Code  | Enter|
+ *                             |      |        | Space|       |      |        |      |
+ *                             `----------------------'       `----------------------'
  */
 
 [BASE] = KEYMAP(  // layer 0 : default
    // left hand
-   KC_GRV,        KC_1,    KC_2,    KC_3, KC_4, KC_5, KC_TRNS,
-   KC_TAB,        KC_Q,    KC_W,    KC_E, KC_R, KC_T, KC_LBRC,
-   CTL_T(KC_ESC), KC_A,    KC_S,    KC_D, KC_F, KC_G,
-   KC_LSFT,       KC_Z,    KC_X,    KC_C, KC_V, KC_B, KC_LPRN,
-   KC_LCTL,       KC_LALT, KC_TRNS, KC_LALT,    KC_LGUI,
+   KC_GRV,  KC_1,    KC_2,    KC_3, KC_4, KC_5, KC_TRNS,
+   KC_TAB,  KC_Q,    KC_W,    KC_E, KC_R, KC_T, KC_LBRC,
+   KC_ESC,  KC_A,    KC_S,    KC_D, KC_F, KC_G,
+   KC_LSFT, KC_Z,    KC_X,    KC_C, KC_V, KC_B, KC_LPRN,
+   KC_LCTL, KC_LALT, KC_TRNS, KC_LALT,    KC_LGUI,
                                         KC_TRNS, KC_TRNS,
                                                  KC_TRNS,
                                 KC_BSPC, KC_DEL, KC_SPC,
@@ -69,12 +65,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                                                 // right hand
                                                                 KC_MINS, KC_6, KC_7,   KC_8,     KC_9,    KC_0,    KC_EQL,
                                                                 KC_RBRC, KC_Y, KC_U,   KC_I,     KC_O,    KC_P,    KC_BSLS,
-                                                                         KC_H, KC_J,   KC_K,     KC_L,    KC_SCLN, CTL_T(KC_QUOT),
+                                                                         KC_H, KC_J,   KC_K,     KC_L,    KC_SCLN, KC_QUOT,
                                                                 KC_RPRN, KC_N, KC_M,   KC_COMM,  KC_DOT,  KC_SLSH, KC_RSFT,
                                                                                KC_SPC, MO(MDIA), KC_RGUI, KC_RALT, KC_RCTL,
-                                                                TG(NUMBER), KC_TRNS,
-                                                                TG(CLMK),
-                                                                TG(MDIA), MO(NUMBER), KC_ENT
+                                                                KC_TRNS, KC_TRNS,
+                                                                KC_TRNS,
+                                                                KC_TRNS, MO(NUMBER), KC_ENT
     ),
 
 /* Keymap 1: Number Layer
