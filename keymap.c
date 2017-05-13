89afb6df460caf98abd9f5b0c4481f3e502a2ad4
Josh Davis <j@joshldavis.com>
2017-05-13 12:32:01 -0700

    Change left space to delete word

--- keymap.c
+++ keymap.c
@@ -47,7 +47,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *                             ,------|--------|------|       |------+--------+------.
  *                             |      |        |      |       |      |        |      |
  *                             |Bkspc |   Del  |------|       |------| ~Code  | Enter|
- *                             |      |        | Space|       |      |        |      |
+ *                             |      |        | ^Bksk|       |      |        |      |
  *                             `----------------------'       `----------------------'
  */
 
@@ -60,7 +60,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    KC_LCTL, KC_LALT, KC_TRNS, KC_LALT,    KC_LGUI,
                                         KC_TRNS, KC_TRNS,
                                                  KC_TRNS,
-                                KC_BSPC, KC_DEL, KC_SPC,
+                                KC_BSPC, KC_DEL, LALT(KC_BSPC),
 
                                                                 // right hand
                                                                 KC_MINS, KC_6, KC_7,   KC_8,     KC_9,    KC_0,    KC_EQL,
